643a780a2db96f4dc9d7fbb3f4ca0eed65846ca1
appleEmily <emiri3128@gmail.com>
2019-11-23 00:55:49 +0900

    Initial Commit

---
+++ Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by 野崎絵未里 on 2019/11/23.
+//  Copyright © 2019 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
